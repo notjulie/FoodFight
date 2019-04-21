46b2629457eb549ea2f4c72fd0f5cf157fe9be6c
Randy Rasmussen <randy@evergreenresearch.com>
2019-04-21 11:27:56 -0500

    First useful test with actual joystick tracking.  Looks solid.

--- VideoJoystick/FrameHandler.cpp
+++ VideoJoystick/FrameHandler.cpp
@@ -5,6 +5,7 @@
  *      Author: pi
  */
 
+#include <algorithm>
 #include "FrameHandler.h"
 
 FrameHandler::FrameHandler(void)
@@ -22,8 +23,45 @@ void FrameHandler::HandleFrame(const std::shared_ptr<VideoFrame> &frame)
 	if (framesWritten >= 20)
 		return;
 
+	// get the red intensity of each pixel... I want to know which pixels are red,
+	// and don't have blue or green, so I just report the difference between red and
+	// the larger of blue or green
+	double xSum = 0;
+	double ySum = 0;
+	double rSum = 0;
+	const uint8_t *p = frame->GetPixelData();
+	auto pixelDataLength = frame->GetPixelDataLength();
+	for (int i=0; i<pixelDataLength; i+=3)
+	{
+		int r = p[0];
+		int g = p[1];
+		int b = p[2];
+		int gray = std::min(g, b);
+		r -= gray;
+		g -= gray;
+		b -= gray;
+
+		int x = (i/3)%640;
+		int y = (i/3)/640;
+
+		if (r >= 3*std::max(g, b))
+		{
+			r = r - 3*std::max(g, b);
+			double weight = r * r;
+			xSum += weight*((i/3)%640);
+			ySum += weight*((i/3)/640);
+			rSum += weight;
+		}
+		p += 3;
+	}
+
+	if (rSum > 0)
+		printf("%d,%d\n", (int)(xSum/rSum), (int)(ySum/rSum));
+
 	int bytes_to_write = frame->GetPixelDataLength();
 	fwrite(frame->GetPixelData(), 1, bytes_to_write, file_handle);
+	//int bytes_to_write = rednessMap.size();
+	//fwrite(&rednessMap[0], 1, bytes_to_write, file_handle);
 	++framesWritten;
 }
 
