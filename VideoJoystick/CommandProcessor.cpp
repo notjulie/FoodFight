6e2c414403b1759b520126c8dafe358cbaebd1f3
Randy Rasmussen <randy@evergreenresearch.com>
2020-05-01 19:51:56 -0500

    Increment... split command line on first space.

--- VideoJoystick/CommandProcessor.cpp
+++ VideoJoystick/CommandProcessor.cpp
@@ -10,8 +10,25 @@ CommandProcessor::CommandProcessor(void)
 }
 
 
-std::string CommandProcessor::ProcessCommand(const std::string &command)
+std::string CommandProcessor::ProcessCommand(const std::string &_command)
 {
-	return "bleem";
+	// split the command into a command and a parameter string
+	std::string command;
+	std::string parameters;
+	auto spacePosition = _command.find(' ');
+	if (spacePosition == std::string::npos)
+	{
+		command = _command;
+	}
+	else
+	{
+		command.append(_command.begin(), _command.begin() + spacePosition);
+		parameters.append(_command.begin() + spacePosition + 1, _command.end());
+	}
+
+	std::string result = "Unknown command: \"";
+	result += command;
+	result += "\"";
+	return result;
 }
 
