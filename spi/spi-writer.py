ea92cba086717b589af53b9c3496d30b4e2445d9
Randy Rasmussen <randy@evergreenresearch.com>
2021-04-11 17:43:03 -0500

    Changed the spi-writer Python app to listen to a named-pipe instead of stdin.

--- spi/spi-writer.py
+++ spi/spi-writer.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python
 
 import spidev
-import sys
 
 # configure SPI
 spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 7629
 
-# process commands from stdin
+# process commands from our named pipe indefinitely
 channel = 0x9000
 value = 0
-for line in sys.stdin:
-     text = line.rstrip()
-     if 'a' == text:
-          channel = 0x9000
-     elif 'b' == text:
-          channel = 0xA000
-     else:
-          value = channel + (4*int(text))
-          msb = value >> 8
-          lsb = value & 0xFF
-          spi.xfer([msb, lsb])
+
+while 1==1:
+	pipe = open('/home/pi/ffJoystick', 'r')
+	for line in pipe:
+	     text = line.rstrip()
+	     if 'a' == text:
+	          channel = 0x9000
+	     elif 'b' == text:
+	          channel = 0xA000
+	     else:
+	          value = channel + (4*int(text))
+	          msb = value >> 8
+	          lsb = value & 0xFF
+	          spi.xfer([msb, lsb])
   
 
 
