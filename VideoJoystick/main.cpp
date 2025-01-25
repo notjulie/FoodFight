0d7e88e242a0398f8191e0099b779c4115faada1
Randy Rasmussen <rasmussenrandy189@gmail.com>
2025-01-25 11:10:12 -0600

    cleaned up main function and signal handling

--- VideoJoystick/main.cpp
+++ VideoJoystick/main.cpp
@@ -4,23 +4,7 @@
 // Warantee: none, your own risk
 //
 
-// We use some GNU extensions (basename)
-#ifndef _GNU_SOURCE
-#define _GNU_SOURCE
-#endif
-
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <memory.h>
-#include <sysexits.h>
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
+#include <csignal>
 #include <iostream>
 #include <stdexcept>
 
@@ -35,12 +19,11 @@
 
 
 extern "C" {
-	static void signal_handler(int signal_number);
+   static void signal_handler(int signal_number);
 };
 
 
-static bool terminateRequested = false;
-
+static volatile std::sig_atomic_t signalStatus = 0;
 
 /**
  * Handler for sigint signals
@@ -50,17 +33,8 @@ static bool terminateRequested = false;
  */
 static void signal_handler(int signal_number)
 {
-	if (signal_number == SIGUSR1)
-	{
-		// Handle but ignore - prevents us dropping out if started in none-signal mode
-		// and someone sends us the USR1 signal anyway
-	}
-	else
-	{
-		// Going to abort on all other signals
-		std::cerr << "Aborting program" << std::endl;
-		terminateRequested = true;
-	}
+   signalStatus = signal_number;
+   std::signal(signal_number, SIG_IGN);
 }
 
 
@@ -69,45 +43,59 @@ static void signal_handler(int signal_number)
  */
 int main(int argc, const char **argv)
 {
-	// create a command handler; we can add commands to it as we go along
-	CommandProcessor commander;
+   // create a command handler; we can add commands to it as we go along
+   CommandProcessor commander;
 
-	// set up our socket listener and tell it how to process commands it receives...
-	// this is basically a TCP command line for diagnostics
-	SocketListener socketListener([&commander](const std::string &s){return commander.ProcessCommand(s);});
+   // set up our socket listener and tell it how to process commands it receives...
+   // this is basically a TCP command line for diagnostics
+   SocketListener socketListener([&commander](const std::string &s)
+   {
+      return commander.ProcessCommand(s);
+   });
 
-	// add our command handlers
-	commander.AddHandler("shutdown", [](std::string){ terminateRequested = true; return std::string(); });
+   // add our command handlers
+   commander.AddHandler("shutdown", [](std::string)
+   {
+      signalStatus = SIGINT;
+      return std::string();
+   });
 
-	// initialize the SPIDAC and add a couple commands
-	SPIDAC spiDac;
-	commander.AddHandler("setX", [&spiDac](std::string param){ spiDac.sendX(atol(param.c_str())); return std::string(); });
-	commander.AddHandler("setY", [&spiDac](std::string param){ terminateRequested = true; spiDac.sendY(atol(param.c_str())); return std::string(); });
+   // initialize the SPIDAC and add a couple commands
+   SPIDAC spiDac;
+   commander.AddHandler("setX", [&spiDac](std::string param)
+   {
+      spiDac.sendX(atol(param.c_str()));
+      return std::string();
+   });
+   commander.AddHandler("setY", [&spiDac](std::string param)
+   {
+      spiDac.sendY(atol(param.c_str()));
+      return std::string();
+   });
 
-	// testing... a loop that lets the SocketListener run but doesn't
-	// bother with any of the camera stuff
+   // testing... a loop that lets the SocketListener run but doesn't
+   // bother with any of the camera stuff
 #if 0
-	for (;;)
-	{
-		if (terminateRequested)
-			return 0;
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-	}
+   for (;;)
+   {
+      if (terminateRequested)
+         return 0;
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+   }
 #endif
 
-	// Our main objects..
-	FrameHandler frameHandler;
+   // Our main objects..
+   FrameHandler frameHandler;
 
-	// command handlers supported by camera functions
-	commander.AddHandler("getImage", [&](std::string){ return frameHandler.GetImageAsString(); });
+   // command handlers supported by camera functions
+   commander.AddHandler("getImage", [&](std::string)
+   {
+      return frameHandler.GetImageAsString();
+   });
 
-	signal(SIGINT, signal_handler);
+   std::signal(SIGINT, signal_handler);
 
-	// Disable USR1 for the moment - may be reenabled if go in to signal capture mode
-	signal(SIGUSR1, SIG_IGN);
-
-
-	// Now set up our components
+   // Now set up our components
    try
    {
       std::unique_ptr<FrameGrabber> frameGrabber(LibCameraFrameGrabber::createUniqueCamera());
@@ -115,17 +103,17 @@ int main(int argc, const char **argv)
       // Enable the camera video port and tell it its callback function
       frameGrabber->SetupFrameCallback([&](const std::shared_ptr<VideoFrame> &frame)
       {
-          // process it
-          frameHandler.HandleFrame(frame);
+         // process it
+         frameHandler.HandleFrame(frame);
       });
 
       // start grabbing frames
       frameGrabber->StartCapturing();
 
       // watch for signal to exit
-      while (!terminateRequested)
+      while (!signalStatus)
       {
-          std::this_thread::sleep_for(std::chrono::milliseconds(100));
+         std::this_thread::sleep_for(std::chrono::milliseconds(100));
       }
    }
    catch (std::exception &e)
@@ -133,6 +121,6 @@ int main(int argc, const char **argv)
       std::cerr << e.what() << std::endl;
    }
 
-	return 0;
+   return 0;
 }
 
